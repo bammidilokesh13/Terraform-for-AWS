3e3aa1d10567594b6c794fc58d049e1acb7a2125
bammidilokesh13 <166898031+bammidilokesh13@users.noreply.github.com>
2024-07-01 12:36:40 +0530

    Update backend.tf

--- Non-Modularized/Two-Tier-Architecture/bloody-sweet/backend.tf
+++ Non-Modularized/Two-Tier-Architecture/bloody-sweet/backend.tf
@@ -6,10 +6,10 @@ terraform {
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
-  required_version = ">=5.56.0"
+  required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">=5.56.0"
+      version = ">= 2.7.0"
       source  = "hashicorp/aws"
     }
   }
